f611dee5d1b20ab028ed7bf8f2e2c70c5451746d
Lewis Moore <lewis.moore2707@gmail.com>
2018-05-16 09:40:19 +0100

    Add functionality when raindrops given 6, return Pling

--- src/raindrops.js
+++ src/raindrops.js
@@ -1,5 +1,6 @@
 class Raindrops {
   raindrops(n){
     if (n == 3) return 'Pling'
+    if (n == 6) return 'Pling'
   }
 }
--- tests/raindropsTest.js
+++ tests/raindropsTest.js
@@ -8,4 +8,8 @@ describe('Raindrops', function(){
   it('takes argument 3 and returns Pling', function(){
     expect(raindrops.raindrops(3)).toEqual('Pling')
   })
+
+  it('takes argument 6 and returns Pling', function(){
+    expect(raindrops.raindrops(6)).toEqual('Pling')
+  })
 })
