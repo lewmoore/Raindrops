b73a11a1a7a18470ad8109cbbc3e4793a2bc6f8d
Lewis Moore <lewis.moore2707@gmail.com>
2018-05-16 10:40:34 +0100

    Refactor Raindrop to evaluate all modulos of 3 and 5 to PlingPlang

--- src/raindrops.js
+++ src/raindrops.js
@@ -1,7 +1,6 @@
 class Raindrops {
   raindrops(n){
-    if (n == 15) return 'PlingPlang'
-    if (n == 30) return 'PlingPlang'
+    if (n % 3 == 0 && n % 5 == 0) return 'PlingPlang'
     if (this._pling(n)) return 'Pling'
     if (this._plang(n)) return 'Plang'
     if (this._plong(n)) return 'Plong'
