184d506aac108ca833c043e19f4dc20a68456c54
Lewis Moore <lewis.moore2707@gmail.com>
2018-05-16 17:21:16 +0100

    Rename functions to improve readability

--- src/raindrops.js
+++ src/raindrops.js
@@ -1,36 +1,36 @@
 class Raindrops {
   raindrops(n){
-    if (this._plangPlong(n)) return 'PlangPlong'
-    if (this._plingPlong(n)) return 'PlingPlong'
-    if (this._plingPlang(n)) return 'PlingPlang'
-    if (this._pling(n)) return 'Pling'
-    if (this._plang(n)) return 'Plang'
-    if (this._plong(n)) return 'Plong'
+    if (this._hasFactorOfFiveAndSeven(n)) return 'PlangPlong'
+    if (this._hasFactorOfThreeAndSeven(n)) return 'PlingPlong'
+    if (this._hasFactorOfThreeAndFive(n)) return 'PlingPlang'
+    if (this._hasFactorOfThree(n)) return 'Pling'
+    if (this._hasFactorOfFive(n)) return 'Plang'
+    if (this._hasFactorOfSeven(n)) return 'Plong'
     if (typeof n != "number") return 'Attempt Failed: Please insert a number'
     return n
   }
 
-  _pling(n){
+  _hasFactorOfThree(n){
     return n % 3 == 0
   }
 
-  _plang(n){
+  _hasFactorOfFive(n){
     return n % 5 == 0
   }
 
-  _plong(n){
+  _hasFactorOfSeven(n){
     return n % 7 == 0
   }
 
-  _plingPlang(n){
+  _hasFactorOfThreeAndFive(n){
     return n % 3 == 0 && n % 5 == 0
   }
 
-  _plingPlong(n){
+  _hasFactorOfThreeAndSeven(n){
     return n % 3 == 0 && n % 7 == 0
   }
 
-  _plangPlong(n){
+  _hasFactorOfFiveAndSeven(n){
     return n % 5 == 0 && n % 7 == 0
   }
 }
